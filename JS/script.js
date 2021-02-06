b18cef572724544c323c6b3b695a64f4c04725bb
Nathan <nls999@live.co.uk>
2021-02-06 18:09:07 +0000

    added features
    Added styling, functionality to ignore certain things, filter out any matches under a certain number

--- JS/script.js
+++ JS/script.js
@@ -6,15 +6,28 @@ function countWords() {
     loopThroughTranscript(transcriptSplit);
 }
 
+function wordsToIgnore() {
+    const TEXTBOX = document.querySelector(".words-to-remove-modal textarea").value;
+    return TEXTBOX.split(/\s+/);
+}
+
 function loopThroughTranscript(transcriptArray){
     var resultArray = [];
 
     for (let i=0; i< transcriptArray.length; i++){
         var wordExists = false;
         var transcriptWord = {
-                                word: transcriptArray[i].toLowerCase(),
+                                word: modifyWord(transcriptArray[i]),
                                 count: 1
                              };
+
+        if (wordsToIgnore().includes(transcriptWord.word)){
+            continue;
+        }
+        
+        if ( transcriptWord.word == "" || transcriptWord.word == " " ){
+            continue;
+        }
         
         if (resultArray.length > 0 ) {                    
             for (let i=0; i < resultArray.length ; i++) {
@@ -37,12 +50,16 @@ function loopThroughTranscript(transcriptArray){
 }
 
 function createResultsTable(resultArray){
-    clearTable();
+    clearTable(false);
 
     const RESULTTABLE = document.querySelector(".results-output #results-table");
+    const MINCOUNT = document.querySelector(".details-to-ignore #minimum-count").value;
 
     //create a table row containing data for each item in result array
     for (let i = 0; i < resultArray.length; i++){
+        if (resultArray[i].count < MINCOUNT){
+            continue;
+        }
         var tableRow = document.createElement("tr");
         
         var wordField = document.createElement("td");
@@ -59,7 +76,7 @@ function createResultsTable(resultArray){
     }
 }
 
-function clearTable(){
+function clearTable(clearTranscript){
     var existingTableRows = document.querySelectorAll("#results-table tr");
     for (let i = 0; i < existingTableRows.length; i++){
         var id = existingTableRows[i].getAttribute("id");
@@ -69,4 +86,31 @@ function clearTable(){
             parentElement.removeChild(existingTableRows[i]);
         }
     }
+
+    if (clearTranscript){
+        document.querySelector(".input-container #transcript").value = "";
+        document.querySelector(".input-container #transcript").focus();
+    }
 }
+
+function modifyWord(word) {
+    var removeSymbols = document.querySelector(".details-to-ignore #symbols").checked;
+    var word = word.toLowerCase();
+
+    if (removeSymbols){
+        word = word.replace(/[^a-zA-Z ]/g,'');
+    }
+    return word;
+}
+
+function showCustomWordsModal(){
+    document.querySelector(".words-to-remove-modal-background").style.display = "flex";
+}
+
+function hideCustomWordsModal(){
+    document.querySelector(".words-to-remove-modal-background").style.display = "none";
+}
+
+document.querySelector(".input-container #transcript").focus();
+
+document.querySelector(".results-output #clear-results").addEventListener("click",function(){clearTable(true)},false);
